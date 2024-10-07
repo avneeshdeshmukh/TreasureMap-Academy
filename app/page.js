753bf4eb3405c9a5a8ff1b54f948e021129826fd
coderkedarxp <kedarchikane21@gmail.com>
2024-10-07 11:58:06 +0530

    Login and Sign Up v1

--- app/page.js
+++ app/page.js
@@ -1,7 +1,20 @@
-import Image from "next/image";
+import Head from "next/head";
+import {
+  FaFacebookF,
+  FaLinkedinIn,
+  FaGoogle,
+  FaEnvelope,
+} from "react-icons/fa";
+import { MdLock } from "react-icons/md";
 
 export default function Home() {
   return (
-    <h1>TreasureMap Academy</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 " style={{backgroundColor: '#efebe2'}}>
+      <Head>
+        <title>TMA</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+    </div>
   );
 }
