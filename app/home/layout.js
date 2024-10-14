52747397a1421b3dac19a75c52538e552cbc2d1d
avneeshdeshmukh <avneesh1745@gmail.com>
2024-10-14 19:40:25 +0530

    Fixed errors

---
+++ app/home/layout.js
@@ -0,0 +1,41 @@
+import localFont from "next/font/local";
+import "../globals.css";
+import Footer from "@/components/homepage/Footer";
+import Homenavbar from "@/components/homepage/Homenavbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const geistSans = localFont({
+    src: "../fonts/GeistVF.woff",
+    variable: "--font-geist-sans",
+    weight: "100 900",
+});
+
+const geistMono = localFont({
+    src: "../fonts/GeistMonoVF.woff",
+    variable: "--font-geist-mono",
+    weight: "100 900",
+});
+
+export const metadata = {
+  title: "TMA - Treasure Map Academy",
+  description:
+    "Gamify your learning experience with TMA where adventure of knowlegde awaits you!",
+};
+
+export default function HomeLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Homenavbar />
+        <div className="min-h-screen">
+          {/* <Homehero /> */}
+          {children}
+        </div>
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ components/homepage/Homenavbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Homenavbar = () => {
+    return (
+        <nav className="bg-yellow-400 text-slate-900 font-semibold text-lg flex justify-center items-center px-14 h-20">
+            <div className="logo flex flex-col justify-center items-center">
+                <span className="flex items-center justify-center gap-3">
+                    <img
+                        src="/images/logo1.png"
+                        alt=""
+                        className="h-14 w-auto object-contain mt-0.5"
+                    />
+                    <h4 className="font-bold mt-2 text-3xl">Tresure Map Academy</h4>
+                </span>
+            </div>
+        </nav>
+    );
+};
+
+export default Homenavbar;
