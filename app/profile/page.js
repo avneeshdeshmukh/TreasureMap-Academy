1d01e863a0359b26f19cb5b4489934c2fd710cfc
Mihika Saraf <mihikaps003@gmail.com>
2024-10-18 20:55:36 +0530

    leaderboard added

---
+++ app/profile/page.js
@@ -0,0 +1,17 @@
+"use client";
+import Sidebar from "@/components/learn/Sidebar";
+import RankCard from "@/components/profile/RankCard"; // Fixed import for RankCard
+import Leaderboard from "@/components/profile/Leaderboard";
+
+export default function Home() {
+    return (
+        <div className="min-h-screen flex" style={{ backgroundColor: "#f5f5dc" }}>
+            <Sidebar />
+            <div className="flex flex-col flex-1 p-3 mb-auto ml-64"> 
+                {/* Added flex-col to stack components vertically */}
+                <RankCard />
+                <Leaderboard /> 
+            </div>
+        </div>
+    );
+}
