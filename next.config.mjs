d861ba9ef675d9a49515626cf6874b7ea2d2993d
avneeshdeshmukh <avneesh1745@gmail.com>
2025-01-20 23:00:31 +0530

    S3 fetch basic 2

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
     images: {
         domains: ['lh3.googleusercontent.com'], // Add the external domain here
     },
+    reactStrictMode : false,
 };
 
 export default nextConfig;
